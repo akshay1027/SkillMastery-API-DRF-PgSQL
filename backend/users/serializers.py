5c89c93a0af98b7b61adb39a60ae1ff4ec80d0c3
Akshay R R <akshayar1027@gmail.com>
2022-01-20 04:06:10 +0530

    User register API, return tokens done

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -26,12 +26,12 @@ class UserProfileSerializer(serializers.ModelSerializer):
         model = User
         fields = '__all__'
 
-    # def get_profile_pic(self, obj):
-    #     try:
-    #         pic = obj.profile_pic.url
-    #     except:
-    #         pic = None
-    #     return pic
+    def get_profile_pic(self, obj):
+        try:
+            pic = obj.profile_pic.url
+        except:
+            pic = None
+        return pic
 
 
 class CurrentUserSerializer(serializers.ModelSerializer):
@@ -60,24 +60,35 @@ class UserSerializer(serializers.ModelSerializer):
     #     return serializer.data
 
 
+# regsuter user
 class UserSerializerWithToken(UserSerializer):
+    print('here')
     access = serializers.SerializerMethodField(read_only=True)
     refresh = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
+        print('here meta')
         model = User
         exclude = ['password']
 
+    def get_profile(self, obj):
+        print('object =', obj)
+        serializer = UserProfileSerializer(obj, many=False)
+        return serializer.data
+
     def get_access(self, obj):
+        print('isnide function')
         token = RefreshToken.for_user(obj)
 
         token['username'] = obj.username
         # token['name'] = obj.userprofile.name
         # token['profile_pic'] = obj.userprofile.profile_pic.url
         # token['is_staff'] = obj.is_staff
-        token['id'] = obj.id
+        token['id'] = str(obj.id)
+        # print()
         return str(token.access_token)
 
     def get_refresh(self, obj):
+
         token = RefreshToken.for_user(obj)
         return str(token)
--- backend/users/views.py
+++ backend/users/views.py
@@ -102,11 +102,3 @@ class RegisterView(APIView):
             return Response({'detail': f'{e}'}, status=status.HTTP_400_BAD_REQUEST)
 
         return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-# @api_view(['GET'])
-# def getNotes(request):
-#     user = request.user
-#     notes = user.note_set.all()
-#     serializer = NoteSerializer(notes, many=True)
-#     return Response(serializer.data)
